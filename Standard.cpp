7c9fc521b22fb2d2ce062df90b741ff03c4a20f2
raymyun <yangraymond2011@gmail.com>
2021-05-17 17:55:56 -0700

    Fixed Checkout Feature

--- Standard.cpp
+++ Standard.cpp
@@ -12,17 +12,20 @@ void Standard::run() {}
 void Standard::recommend() {}
 void Standard::checkoutBook(Book* b) {
 	bool bookFound = false;
+	int index = 0;
 	for (int i = 0; i < library.size(); i++) {
 		if (library.at(i)->GetID() == b->GetID()) {
-			library.erase(library.begin() + i - 1);
+			index = i;
 			bookFound = true;
 		}
 	}
 	if (bookFound) {
+		library.erase(library.begin() + index - 1);
 		checkedOut.push_back(b);
+		std::cout << "Book with ID: " << b->GetID() << " has been successfully checked out." << endl;
 	}
 	else {
-		//Book was not found in Library.
+		std::cout << "Book with ID: " << b->GetID() << " was not found in the Library." << endl;
 	{
 }
 void Standard::returnBook(Book* b) {
