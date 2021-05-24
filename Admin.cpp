d614a7b14f559357b32d3559ef9df6c4211bebd0
puloma-k <puloma@pulomakatiyar.com>
2021-05-24 00:19:52 -0700

    Login feature working, changed function definitions

---
+++ Admin.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include "Library.hpp"
+#include "Admin.hpp"
+#include <iostream>
+
+Admin::Admin(const std::string& uname, const std::string& pass) : Standard(uname, pass) {}
+
+void Admin::run(){}
+void Admin::addBook(Book* b, std::vector<Book*> library) {
+	library.push_back(b);
+	std::cout << "Book was successfully added to Library." << std::endl;
+}
+void Admin::remBook(Book* b, std::vector<Book*> library) {
+	bool bookFound = false;
+	int index = 0;
+	for (int i = 0; i < library.size(); i++) {
+		if (library.at(i)->GetID() == b->GetID()) {
+			index = i;
+			bookFound = true;
+		}
+	}
+	if (bookFound) {
+		library.erase(library.begin() + index - 1);
+		std::cout << "Book was successfully removed from Library." << std::endl;
+	}
+	else {
+		std::cout << "Book was not found in Library." << std::endl;
+	}
+}
