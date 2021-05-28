6b5c87deefd48dbf2698c8c1d0107df8584197a7
raymyun <yangraymond2011@gmail.com>
2021-05-28 01:16:08 -0700

    Created all test files and fixed header guard of Library.hpp

---
+++ Admin_Test.hpp
@@ -0,0 +1,11 @@
+#ifndef __ADMIN_TEST_HPP__
+#define __ADMIN_TEST_HPP__
+
+#include "gtest/gtest.h"
+#include "Admin.hpp"
+
+TEST(AdminTest, temp) {
+
+}
+
+#endif
---
+++ Lists_Test.hpp
@@ -0,0 +1,12 @@
+#ifndef __LISTS_TEST_HPP__
+#define __LISTS_TEST_HPP__
+
+#include "gtest/gtest.h"
+#include "Lists.hpp"
+
+TEST(ListsTest, temp) {
+
+}
+
+#endif
+
