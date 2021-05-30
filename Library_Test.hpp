f93b9cae5b2e6faf212f251a563f86a830a48b25
raymyun <yangraymond2011@gmail.com>
2021-05-29 18:38:09 -0700

    Undefined reference to Library?

--- Library_Test.hpp
+++ Library_Test.hpp
@@ -1,11 +1,13 @@
 #ifndef __LIBRARY_TEST_HPP__
 #define __LIBRARY_TEST_HPP__
 
+#include <iostream>
 #include "gtest/gtest.h"
 #include "Library.hpp"
 
 TEST(LibraryTest, temp) {
-
+	//Library* lib = new Library();
+	//Book b = Book("stri", "strin", "string", 0, 0, 0);
 }
 
 #endif
