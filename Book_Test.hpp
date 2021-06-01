05d2669eab1fdadc5c37a025c705c3be5276faa3
raymyun <yangraymond2011@gmail.com>
2021-05-31 17:16:09 -0700

    Book_Test complete.

--- Book_Test.hpp
+++ Book_Test.hpp
@@ -10,6 +10,8 @@ TEST(BookTest, TitleGetter) {
 	//Tests the GetTitle() function
 	
 	EXPECT_EQ(testBook->GetTitle(), "title");
+	
+	delete testBook;
 }
 
 TEST(BookTest, AuthorGetter) {
@@ -18,6 +20,8 @@ TEST(BookTest, AuthorGetter) {
         //Tests the GetAuthor() function
 
         EXPECT_EQ(testBook->GetAuthor(), "author");
+	
+	delete testBook;
 }
 
 TEST(BookTest, GenreGetter) {
@@ -26,6 +30,8 @@ TEST(BookTest, GenreGetter) {
         //Tests the GetGenre() function
 
         EXPECT_EQ(testBook->GetGenre(), "genre");
+
+	delete testBook;
 }
 
 TEST(BookTest, IDGetter) {
@@ -34,6 +40,8 @@ TEST(BookTest, IDGetter) {
         //Tests the GetID() function
 
         EXPECT_EQ(testBook->GetID(), 222);
+	
+	delete testBook;
 }
 
 TEST(BookTest, RatingGetter) {
@@ -42,6 +50,8 @@ TEST(BookTest, RatingGetter) {
         //Tests the GetRating() function
 
         EXPECT_EQ(testBook->GetRating(), 0);
+
+	delete testBook;
 }
 
 TEST(BookTest, NumReviewGetter) {
@@ -50,7 +60,74 @@ TEST(BookTest, NumReviewGetter) {
         //Tests the GetNumReviews() function
 
         EXPECT_EQ(testBook->GetNumReviews(), 0);
+
+	delete testBook;
 }
 
+TEST(BookTest, TitleSetter) {
+	Book* testBook = new Book("title", "author", "genre", 222, 0, 0);
+	testBook->SetTitle("newTitle");
+
+	//Tests the SetTitle() function
+
+	EXPECT_EQ(testBook->GetTitle(), "newTitle");
+
+	delete testBook;
+}
+
+TEST(BookTest, AuthorSetter) {
+        Book* testBook = new Book("title", "author", "genre", 222, 0, 0);
+        testBook->SetAuthor("newAuthor");
+
+        //Tests the SetAuthor() function
+
+        EXPECT_EQ(testBook->GetAuthor(), "newAuthor");
+
+        delete testBook;
+}
+
+TEST(BookTest, GenreSetter) {
+        Book* testBook = new Book("title", "author", "genre", 222, 0, 0);
+        testBook->SetGenre("newGenre");
+
+        //Tests the SetGenre() function
+
+        EXPECT_EQ(testBook->GetGenre(), "newGenre");
+
+        delete testBook;
+}
+
+TEST(BookTest, IDSetter) {
+        Book* testBook = new Book("title", "author", "genre", 222, 0, 0);
+        testBook->SetID(333);
+
+        //Tests the SetID() function
+
+        EXPECT_EQ(testBook->GetID(), 333);
+
+        delete testBook;
+}
+
+TEST(BookTest, RatingSetter) {
+        Book* testBook = new Book("title", "author", "genre", 222, 0, 0);
+        testBook->SetRating(1);
+
+        //Tests the SetRating() function
+
+        EXPECT_EQ(testBook->GetRating(), 1);
+
+        delete testBook;
+}
+
+TEST(BookTest, NumReviewsSetter) {
+        Book* testBook = new Book("title", "author", "genre", 222, 0, 0);
+        testBook->SetNumReviews(1);
+
+        //Tests the SetNumReviews() function
+
+        EXPECT_EQ(testBook->GetNumReviews(), 1);
+
+        delete testBook;
+}
 
 #endif
